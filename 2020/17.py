9f34ecf734bbb55a303e831cf3caddb56c50b420
Ryan Pearl <rpearl@endofunctor.org>
2020-12-16 22:52:19 -0800

    much faster, and clearer

--- 2020/17.py
+++ 2020/17.py
@@ -6,6 +6,8 @@ from sortedcontainers import SortedList, SortedDict, SortedSet
 import itertools
 import u
 import math
+import time
+import operator
 
 
 def nosubmit(answer, part):
@@ -23,91 +25,58 @@ data = """.#.
 print(f"File line count: {len(lines)}")
 
 
-def a():
-    grid = {}
+@functools.cache
+def neighbors(pos):
+    dim = len(pos)
+    return [
+        tuple(p + d for p, d in zip(pos, delta))
+        for delta in itertools.product((-1, 0, 1), repeat=dim)
+        if any(d != 0 for d in delta)
+    ]
+
+
+def run(n):
+    active = set()
     for y in range(len(lines)):
         for x in range(len(lines[0])):
+            pos = tuple([x, y] + [0] * (n - 2))
             if lines[y][x] == "#":
-                grid[x, y, 0] = 1
-
-    def neighbors(pos):
-        x, y, z = pos
-        for dx, dy, dz in itertools.product((-1, 0, 1), repeat=3):
-            if dx == dy == dz == 0:
-                continue
-            yield (x + dx, y + dy, z + dz)
-
+                active.add(pos)
     for step in range(6):
-        ngrid = {}
-        lx = min(p[0] for p in grid)
-        hx = max(p[0] for p in grid)
-        ly = min(p[1] for p in grid)
-        hy = max(p[1] for p in grid)
-        lz = min(p[2] for p in grid)
-        hz = max(p[2] for p in grid)
-        for z in range(lz - 1, hz + 2):
-            for y in range(ly - 1, hy + 2):
-                for x in range(lx - 1, hx + 2):
-                    pos = x, y, z
-                    cur = grid.get(pos)
-                    c = sum(grid.get(npos, 0) for npos in neighbors(pos))
-                    if cur == 1:
-                        ngrid[pos] = 1 if c in {2, 3} else 0
-                    else:
-                        ngrid[pos] = 1 if c == 3 else 0
-        grid = ngrid
-    return sum(grid.values())
-    pass
+        counts = Counter()
+        for pos in active:
+            for npos in neighbors(pos):
+                counts[npos] += 1
+        active = {
+            pos for pos, c in counts.items() if c == 3 or (c == 2 and pos in active)
+        }
+
+    return len(active)
+
+
+def a():
+    return run(3)
 
 
 def b():
-    grid = defaultdict(int)
-    for y in range(len(lines)):
-        for x in range(len(lines[0])):
-            if lines[y][x] == "#":
-                grid[x, y, 0, 0] = 1
-
-    def neighbors(pos):
-        x, y, z, w = pos
-        for dx, dy, dz, dw in itertools.product((-1, 0, 1), repeat=4):
-            if dx == dy == dz == dw == 0:
-                continue
-            yield (x + dx, y + dy, z + dz, w + dw)
-
-    for step in range(6):
-        ngrid = defaultdict(int)
-        lx = min(p[0] for p in grid)
-        hx = max(p[0] for p in grid)
-        ly = min(p[1] for p in grid)
-        hy = max(p[1] for p in grid)
-        lz = min(p[2] for p in grid)
-        hz = max(p[2] for p in grid)
-        lw = min(p[3] for p in grid)
-        hw = max(p[3] for p in grid)
-        for w in range(lw - 1, hw + 2):
-            for z in range(lz - 1, hz + 2):
-                for y in range(ly - 1, hy + 2):
-                    for x in range(lx - 1, hx + 2):
-                        pos = x, y, z, w
-                        c = sum(grid[npos] for npos in neighbors(pos))
-                        if grid[pos] == 1:
-                            ngrid[pos] = 1 if c in {2, 3} else 0
-                        else:
-                            ngrid[pos] = 1 if c == 3 else 0
-        grid = ngrid
-    return sum(grid.values())
-    pass
+    return run(4)
 
 
 def main():
+    astart = time.perf_counter()
     ra = a()
+    aend = time.perf_counter()
     if ra is not None:
         print(ra)
+        print(f"Time taken: {aend-astart:.4f} sec")
         submit(ra, part="a")
 
+    bstart = time.perf_counter()
     rb = b()
+    bend = time.perf_counter()
     if rb is not None:
         print(rb)
+        print(f"Time taken: {bend-bstart:.4f} sec")
         submit(rb, part="b")
 
 
