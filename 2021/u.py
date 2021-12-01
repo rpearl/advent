a52f6918a009ef94df9c32f99348e4acebd84b06
Ryan Pearl <rpearl@endofunctor.org>
2021-11-30 21:40:38 -0800

    u.window

--- 2021/u.py
+++ 2021/u.py
@@ -35,6 +35,17 @@ def hex_axial_distance(a, b):
 def lmap(fn, *its):
     return list(map(fn, *its))
 
+def window(seq, n=2):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    it = iter(seq)
+    result = tuple(itertools.islice(it, n))
+    if len(result) == n:
+        yield result
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
 
 def ints(line):
     return lmap(int, re.findall(r"-?\d+", line))
