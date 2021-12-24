c7befb4b5df1e6a14dc20f1984e108697aa23c9d
Ryan Pearl <rpearl@endofunctor.org>
2021-12-24 10:40:19 -0800

    23, slow as hell

--- 2021/23.py
+++ 2021/23.py
@@ -9,6 +9,7 @@ import u
 import math
 import time
 import operator
+from tqdm import tqdm
 
 ints = u.ints(data)
 intlines = u.lmap(u.ints, lines)
@@ -20,9 +21,20 @@ target = [
     "#############",
     "#...........#",
     "###A#B#C#D###",
+#    "  #A#B#C#D#",
+#    "  #A#B#C#D#",
     "  #A#B#C#D#",
     "  #########",
 ]
+
+#lines = lines[:3] + [
+#"  #D#C#B#A#",
+#"  #D#B#A#C#",
+#] + lines[3:]
+
+#print(lines)
+
+#lines = lines[:3
 
 def freeze(state):
     return frozenset(state.items())
@@ -39,67 +51,74 @@ def build_state(m):
                 grid[x,y] = c
     return freeze(grid)
 
-target_state = build_state(target)
-target_grid = unfreeze(target_state)
-initial_state = build_state(lines)
-room_positions = {(x,y) for (x,y),c in target_grid.items() if c in weightmap}
-doorway_positions = {(x,1) for x, _ in room_positions}
-hallway_positions = set(target_grid.keys()) - room_positions - doorway_positions
 
 def print_grid(grid):
-    mutable = [list(l) for l in target]
-    for (x,y),c in grid.items():
-        mutable[y][x] = c
-    for line in mutable:
-        print(''.join(line))
+    width = max(x for x,_ in grid)
+    height = max(y for _,y in grid)
+    for y in u.incrange(height):
+        for x in u.incrange(width):
+            c = grid.get((x,y), ' ')
+            print(c, end='')
+        print()
+    print()
 
-def make_move(grid, src, dst):
-    newgrid = dict(grid)
-    c = newgrid[src]
-    newgrid[src] = '.'
-    newgrid[dst] = c
-    return freeze(newgrid)
+def solve(init, end):
+    target_state = build_state(end)
+    target_grid = unfreeze(target_state)
+    initial_state = build_state(init)
+    room_positions = {(x,y) for (x,y),c in target_grid.items() if c in weightmap}
+    doorway_positions = {(x,1) for x, _ in room_positions}
+    hallway_positions = set(target_grid.keys()) - room_positions - doorway_positions
+    def make_move(grid, src, dst):
+        newgrid = dict(grid)
+        c = newgrid[src]
+        newgrid[src] = '.'
+        newgrid[dst] = c
+        return freeze(newgrid)
 
-def room_occupants(grid, p):
-    x, y1 = p
-    y2 = 2 if y == 3 else 3
+    def room_occupants(grid, p):
+        x, _ = p
+        return {grid[r] for r in room_positions if r[0] == x}
 
-def reachable(grid, src, c):
-    def neighbors(n):
-        for neighbor in u.orthogonal(grid, n):
-            if grid[neighbor] == '.':
-                yield neighbor
-    _, dists = u.bfs(src, neighbors)
+    def reachable(grid, src, c):
+        def neighbors(n):
+            for neighbor in u.orthogonal(grid, n):
+                if grid[neighbor] == '.':
+                    yield neighbor
+        _, dists = u.bfs(src, neighbors)
 
-    def allowed(dst):
-        if src in room_positions:
-            return dst in hallway_positions
-        else:
-            if target_grid[dst] != c:
-                return False
-            x, y1 = dst
-            y2 = 2 if y1 == 3 else 3
-            return grid[x,y2] in {'.',c}
-    wt = weightmap[c]
-    return ((make_move(grid, src, p), wt*dist) for p, dist in dists.items() if allowed(p))
+        def allowed(dst):
+            if src in room_positions:
+                return dst in hallway_positions
+            else:
+                if target_grid[dst] != c:
+                    return False
+                return room_occupants(grid, dst).issubset({'.',c})
+        wt = weightmap[c]
+        return ((make_move(grid, src, p), wt*dist) for p, dist in dists.items() if allowed(p))
 
-def moves(state):
-    grid = unfreeze(state)
-    for p, c in state:
-        if c in weightmap:
-            yield from reachable(grid, p, c)
+    with tqdm() as pbar:
+        def moves(state):
+            grid = unfreeze(state)
+            for p, c in state:
+                if c in weightmap:
+                    for c in reachable(grid, p, c):
+                        pbar.update(1)
+                        yield c
 
-def a():
-    return
-    #for newstate, _ in moves(initial_state):
-    #    print_grid(unfreeze(newstate))
-    #    print()
-    #return
-    _, dists = u.dijkstra(initial_state, moves, target_state)
+        _, dists = u.dijkstra(initial_state, moves, target_state)
     return dists[target_state]
 
+def a():
+    return solve(lines, target)
 
 def b():
-    pass
+    new_initial = lines[:3] + [
+        "  #D#C#B#A#",
+        "  #D#B#A#C#",
+    ] + lines[3:]
+
+    new_target = target[:3] + [target[3], target[3]] + target[3:]
+    return solve(new_initial, new_target)
 
 u.main(a, b, submit=globals().get('submit', False))
